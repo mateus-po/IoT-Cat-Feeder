272bd64ef3ea52de873e7d20c228317cb6191d4e
Mateusz Porębski <mateusz.porebski99@gmail.com>
2024-12-14 17:54:01 +0100

    create SSD1306_OLED display library

--- main.ino
+++ main.ino
@@ -1,56 +1,17 @@
-#include <HTTPClient.h>
-#include <WiFi.h>
 
-const char* ssid = "realme 9 Pro+";
-const char* password = "mojewifi";
-int LED_BUILTIN = 2;
-String host = "http://catfact.ninja";
-String url = "/fact";
+#define OLED_I2C_ADDRESS 0x3C
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 32
 
-void setup(){
-    Serial.begin(115200);
-    delay(1000);
+#include "SSD1306_OLED.h"
 
-    WiFi.begin(ssid, password);
-    Serial.println("\nConnecting");
+SSD1306_OLED oled(OLED_I2C_ADDRESS, OLED_WIDTH, OLED_HEIGHT);
 
-    while(WiFi.status() != WL_CONNECTED){
-        Serial.print(".");
-        delay(100);
-    }
-
-    Serial.println("\nConnected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
-
-    pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+    oled.begin();
+    oled.clearDisplay();
 }
 
-void loop(){
-  if (WiFi.status() != WL_CONNECTED) {
-      WiFi.begin(ssid, password);
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(1000);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(1000);
-      return;
-  }
-  HTTPClient http;
-  String serverPath = host + url;
-  Serial.println(serverPath);
-  http.begin(serverPath.c_str());
-  int httpResponseCode = http.GET();
-
-  if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    String payload = http.getString();
-    Serial.println(payload);
-  }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  http.end();
-  delay(5000);
+void loop() {
+  oled.displayDemo();
 }
